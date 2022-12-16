73a6c2fdc4aacfb05f662fa940d3cda097e57b86
Anton Boiko <bo1ko.ant@yandex.com>
2022-12-17 00:29:20 +0300

    added Grou import and now all the new users are automatically added into customer group. Also, they can't see Products and Dashboard in navbar anymore, only admin can see it

--- students/views.py
+++ students/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import Group
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import *
@@ -16,9 +17,13 @@ def registerPage(request):
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for ' + user)
+            user = form.save()
+            username = form.cleaned_data.get('username')
+
+            group = Group.objects.get(name='customer')
+            user.groups.add(group)
+
+            messages.success(request, 'Account was created for ' + username)
             return redirect('login')
 
     context = {'form': form}
