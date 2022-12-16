2f5a5d5e269774c7da819b4d4f7b59a7c2eb7a5d
Anton Boiko <bo1ko.ant@yandex.com>
2022-12-16 23:16:35 +0300

    created unathenticated_user decorator to simplify loginPage functionality

---
+++ students/decorators.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import redirect 
+
+
+def unauthenticated_user(view_func):
+    def wrapper_function(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        else:
+            return(view_func(request, *args, **kwargs))
+    return wrapper_function
+    
+# view_funct is executed if user is not authenticated
--- students/views.py
+++ students/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .models import *
+from .decorators import *
 from .forms import OrderForm, CustomerForm, CreateUserForm
 from .filters import OrderFilter
 
@@ -25,25 +26,22 @@ def registerPage(request):
         context = {'form': form}
         return render(request, 'register.html', context)
 
-
+@unauthenticated_user
 def loginPage(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-    else:
-        if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
 
-            user = authenticate(request, username=username, password=password)
+        user = authenticate(request, username=username, password=password)
 
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-            else:
-                messages.info(request, 'Username OR Password is incorrect')
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR Password is incorrect')
 
-        context = {}
-        return render(request, 'login.html', context)
+    context = {}
+    return render(request, 'login.html', context)
 
 
 def logoutUser(request):
