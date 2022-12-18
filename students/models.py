839c78158a5cb93230b7d373246a6fdb10e9a528
Anton Boiko <bo1ko.ant@yandex.com>
2022-12-18 23:31:02 +0300

    started working on pictures for user profiles

--- students/models.py
+++ students/models.py
@@ -8,6 +8,7 @@ class Customer(models.Model):
     phone = models.CharField(max_length=50, null=True)
     email = models.EmailField(max_length=50, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    profile_picture = models.ImageField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     
     class Meta:
