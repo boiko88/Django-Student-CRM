dcc0a479a8a5e60cdca867b6b0cbfcce6916cbfa
Anton Boiko <bo1ko.ant@yandex.com>
2022-12-18 23:43:01 +0300

    modified urls document so images are now displayed via simple links in the project

--- CRM/urls.py
+++ CRM/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from students import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -20,3 +23,5 @@ urlpatterns = [
     path('create-customer/', views.createCustomer, name="create-customer"),
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
